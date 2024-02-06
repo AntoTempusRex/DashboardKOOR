4baac7d835e6d812459a10bce07fcfc3243e68a3
pustikom <tempusrex08@gmail.com>
2024-02-06 13:45:40 +0700

    login commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'unj': ['Praise'],  
+      },
+      
+      screens: {
+        'desktop': '1280px',
+      },
+      backgroundImage: {
+        'unjbg': "url('./img/loginbg.png')",
+      },
+      borderRadius: {
+        'chonk': '20px',
+        '10pixel': '10px',
+      },
+      colors: {
+        ijounj: '#266B45',
+        whiteblurry: 'rgb(255, 255, 255, 76%)',
+        buttonunj: '#305067',
+      },
+      opacity: {
+        '76': '76%',
+        '67': '67%',
+      },
+      borderWidth: {
+        '80%': '100px',
+      },
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
