f3adc62a0228f394ee8c384ec58841567a7685cb
AntoTempusRex <tempusrex08@gmail.com>
2024-02-16 14:05:34 +0700

    whatever commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,7 +54,7 @@ module.exports = {
 				ssm: '1px',
 			},
 			boxShadow: {
-				'all-side': '0 0 60px -15px rgba(0, 0, 0, 0.5)',
+				'all-side': '-10px -10px 30px 4px rgba(0, 0, 0, 0.3)',
 			},
 		},
 	},
