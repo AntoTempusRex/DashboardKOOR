9da35c73934ee7731745180480dbe1703bb64a75
AntoTempusRex <tempusrex08@gmail.com>
2024-02-23 11:34:38 +0700

    DatRuang+Admin Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,18 +39,11 @@ module.exports = {
 				"3xl": "0 35px 35px rgba(0, 0, 0, 0.25)",
 			},
 			gridTemplateColumns: {
-				sidebar: "1.5fr 1fr 1fr 1fr 1fr",
-				testcol: "280px 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
-				contentcol: "1fr 1fr 1fr 1fr 1fr 1fr 1fr",
-				contentcolpinjaman: "1fr 1fr 1fr 1fr 1fr 1fr 1fr",
-				contentcoldlm: "1fr 1fr",
+				DaRef: "1fr 1fr",
+				
 			},
 			gridTemplateRows: {
-				MainMenuHead: "82px 1fr",
-				testrow: "80px 1fr 1fr 1fr 1fr 1fr 1fr 80px",
-				contentrowpinjaman: "1.2fr 1.2fr 1.2fr 1.2fr 1.2fr 1.2fr",
-				contentrow: "1fr 1.2fr 1.2fr 1.2fr 1.2fr 1.2fr",
-				contentrowdlm: "1fr 1fr",
+				DaRef: "1fr",
 			},
 			blur: {
 				ssm: '1px',
